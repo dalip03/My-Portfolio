73b2852519aacb598058c4a91c5080ca90b9f900
Dalip <139840565+dalip04@users.noreply.github.com>
2024-01-21 13:38:44 +0530

    new commit

--- public/src/data/headerData.js
+++ public/src/data/headerData.js
@@ -1,7 +1,7 @@
 
 export const headerData = {
     name: 'DALIP KUMAR',
-    title: "Hello! I am",
+    title: "Hello! I am ",
     desciption: "Hello! I am DALIP KUMAR, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: 'https://i.ibb.co/gPsMSv6/pass-size-photo.jpg',
     imagebw: 'https://i.ibb.co/gPsMSv6/pass-size-photo.jpg',
